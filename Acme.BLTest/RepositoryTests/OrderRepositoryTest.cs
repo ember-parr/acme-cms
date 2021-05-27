01695bcdddb21b51fba429e72de248471d9944e2
Ember Parr <ember-devops@gmail.com>
2021-05-27 11:55:28 -0500

    added unit tests for valid save on Order repository

--- Acme.BLTest/RepositoryTests/OrderRepositoryTest.cs
+++ Acme.BLTest/RepositoryTests/OrderRepositoryTest.cs
@@ -23,5 +23,41 @@ namespace Acme.BLTest.RepositoryTests
             // Assert
             Assert.True(expected.OrderDate == actual.OrderDate);
         }
+
+
+        [Fact]
+        public void SaveTestValid()
+        {
+            // Arrange
+            var orderRepo = new OrderRepository();
+            var updatedOrder = new Order(18)
+            {
+                OrderDate = new DateTimeOffset(DateTime.Now.Year, 4, 14, 10, 00, 00, new TimeSpan(7, 0, 0)),
+                HasChanges = true
+            };
+
+            // Act
+            var actual = orderRepo.Save(updatedOrder);
+
+            // Assert
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void SaveTestMissingOrderDate()
+        {
+            // Arrange
+            var orderRepo = new OrderRepository();
+            var updatedOrder = new Order(24)
+            {
+                HasChanges = true
+            };
+
+            // Act
+            var actual = orderRepo.Save(updatedOrder);
+
+            // Assert
+            Assert.False(actual);
+        }
     }
 }
